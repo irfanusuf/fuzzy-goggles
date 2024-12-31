7b306cb98cfb8f535b02fe7ffc2b6efdfedf8b6b
Irfan usuf <irfanusuf33@gmail.com>
2024-12-31 23:38:34 +0530

    Update User.cs

--- WebApplication1/Models/User.cs
+++ WebApplication1/Models/User.cs
@@ -1,12 +1,28 @@
 using MongoDB.Bson;
+using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication1.Models
 {
     public class User
     {
-        public ObjectId Id { get; set; } 
-        public required string Username { get; set; }
-        public required string Email { get; set; }
-        public required string Phone { get; set; }
+        public ObjectId Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [JsonIgnore]
+        public string Password { get; set; }
+
+        public string? Phone { get; set; }
+        public DateTime UserDateCreated { get; set; }
+        public DateTime? UserDateModified { get; set; }
+        public string? Role { get; set; }
+        public string? ProfilePictureUrl { get; set; }
     }
 }
