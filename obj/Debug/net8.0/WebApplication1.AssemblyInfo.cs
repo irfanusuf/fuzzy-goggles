36c38c5c0da22cb2e6be1c9dd91d9f3a7101bce3
irfan_usuf <irfanusuf33@gmail.com>
2025-01-10 16:39:40 +0530

    env

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7acb362555079bce96c83c048c120f09faa8594")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7130d3c86c01edf57941ce92d277d42d8cf6d8ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
